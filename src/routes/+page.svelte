3bff649bed69e83e22dece36ce834e8de956b480
Dominiku04 <157432853+Dominiku04@users.noreply.github.com>
2025-05-21 14:56:58 +0800

    Flix

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { Card } from "flowbite-svelte";
-</script>
-
-<main class="flex h-screen w-screen flex-col items-center justify-center p-4">
-	<Card size="lg">Test</Card>
-</main>
